fead82c75aca22bb8982a4b6df43cafa41ccd5af
applefi87 <werrtyyui@yahoo.com.tw>
2022-06-17 05:10:13 +0800

    確認陣列+面板內容(8);新增.env(12)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,10 @@
 
 <script setup>
 import { reactive } from 'vue'
+const time = parseInt(import.meta.env.VITE_TIME)
+const rest = parseInt(import.meta.env.VITE_REST)
+const bigRest = parseInt(import.meta.env.VITE_BIG_REST)
+
 
 </script>
  
