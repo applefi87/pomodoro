1e6dda29ef3c04311cec2421edb74e38eeaf2b7f
applefi87 <werrtyyui@yahoo.com.tw>
2022-06-18 22:07:07 +0800

    完成分心功能(40m)

--- src/stores/data.js
+++ src/stores/data.js
@@ -3,15 +3,15 @@ import { defineStore } from 'pinia'
 
 export const useDataStore = defineStore('data',{
   state: () => ({
-      lists:[{ id: 1, name: '製作表單', project: "Vue", totalTime: 0, pauseTime: 0, },{ id: 2, name: '製作表單2', project: "Vue", totalTime: 0, pauseTime: 0, },{ id: 3, name: '製作表單3', project: "Vue", totalTime: 0, pauseTime: 0, }],
+      lists:[{ id: 1, name: '製作表單', project: "Vue", totalTime: 0, distractTime: 0, },{ id: 2, name: '製作表單2', project: "Vue", totalTime: 0, distractTime: 0, },{ id: 3, name: '製作表單3', project: "Vue", totalTime: 0, distractTime: 0, }],
       endLists:[],
-      restLists:[{ id: -1, name: '小休息', project: "", totalTime: 0, pauseTime: 0, },{ id: -2, name: '大休息', project: "", totalTime: 0, pauseTime: 0, },{ id: -3, name: '結束', project: "", totalTime: 0, pauseTime: 0, }],
+      restLists:[{ id: -1, name: '小休息', project: "", totalTime: 0, distractTime: 0, },{ id: -2, name: '大休息', project: "", totalTime: 0, distractTime: 0, },{ id: -3, name: '結束', project: "", totalTime: 0, distractTime: 0, }],
       id:4
     }),
     actions:{
       addlist(name,project){
         this.lists.push(
-          {id:this.id++,name,project,totalTime:0,pauseTime:0,}
+          {id:this.id++,name,project,totalTime:0,distractTime:0,}
         )
       }
     }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,16 @@ h1(v-else)  名稱: {{ nowItem.name }}
 hr
 h1.text-center
 v-row.text-center
-  v-col.timer  {{ leftTimeDisplay }}+{{ leftTime }} 
-  v-col(cols='12')
+  v-col(cols='8').timer  {{ leftTimeDisplay }} 
+  v-col(cols='4' v-if="going && playState === '專心中'").timer 分心:{{ distractTimeDisplay }}
+  v-col(cols='8' )
     v-btn.mx-3(v-if="!going" icon='mdi-play' @click="start")
-    v-btn.mx-3(icon='mdi-pause' @click='stop')
+    v-btn.mx-3(v-else icon='mdi-pause' @click='stop')
     v-btn.mx-3(icon='mdi-skip-next' @click='earlier')
+  v-col(cols='4' v-if="going && playState === '專心中'")
+    v-btn.mx-3(v-if="going && !distracting" icon='mdi-play' @click="distract")
+    v-btn.mx-3(v-if="going && distracting" icon='mdi-pause' @click='stopDistract')
+    //- v-btn.mx-3(icon='mdi-skip-next' @click='earlier')
 </template>
 
 <script setup>
@@ -21,26 +26,31 @@ const time = parseInt(import.meta.env.VITE_TIME)
 const rest = parseInt(import.meta.env.VITE_REST)
 const bigRest = parseInt(import.meta.env.VITE_BIG_REST)
 const nowItem = reactive(dataList.lists[0])
-const leftTime = ref(0)
+let leftTime = 0
+let distractTime = 0
 const leftTimeDisplay = ref('00:00')
+const distractTimeDisplay = ref('00:00')
 const timeFormated = (n) => {
   return Math.floor(n / 60).toString().padStart(2, '0') + ':' + (n % 60).toString().padStart(2, '0')
 }
 
 const going = ref(false)
+const distracting = ref(false)
 const pauseTime = ref(0)
 // '專心中'/'休息中'/'大休息'
 const playState = ref('專心中')
 let timer = 0
-
+let distractTimer = 0
 const start = () => {
   going.value = true
+  distractTime = 0
   if (playState.value === '專心中') {
-    leftTime.value = time
+    leftTime = time
     Object.assign(nowItem, dataList.lists.shift())
   } else if (playState.value === '休息中') {
-    leftTime.value = rest
+    leftTime = rest
     Object.assign(nowItem, dataList.restLists[0])
+    // 清單空了，跳成結束畫面停止執行
     if (dataList.lists.length == 0) {
       Object.assign(nowItem, dataList.restLists[2])
       return false
@@ -48,23 +58,37 @@ const start = () => {
   } else {
     Object.assign(nowItem, dataList.restLists[1])
   }
-  leftTimeDisplay.value = timeFormated(leftTime.value)
+  // 先存進去該次的總計時 避免過程改掉抓到新的
+  nowItem.totalTime = time
+  // 當下立刻更新顯示時間
+  leftTimeDisplay.value = timeFormated(leftTime)
+  distractTimeDisplay.value = timeFormated(distractTime)
   countDown()
 }
+
 const countDown = () => {
   timer = setInterval(() => {
-    leftTimeDisplay.value = timeFormated(--leftTime.value)
-    if (leftTime.value === 0) {
+    leftTimeDisplay.value = timeFormated(--leftTime)
+    if (leftTime === 0) {
       end(true)
     }
   }, 1000)
 }
 const end = (b) => {
+  // 停止全部的計時
   clearInterval(timer)
+  clearInterval(distractTimer)
+  // 更新專心時間-分心時間
+  nowItem.totalTime = nowItem.totalTime - distractTime
+  // 更新分心時間+關閉分心狀態
+  nowItem.distractTime = distractTime
+  distracting.value = false
+
+  // 當前加到結束去
   dataList.endLists.push(nowItem)
-  console.log(dataList.endLists.length);
+  console.log(dataList.endLists[dataList.endLists.length - 1]);
   if (b) (alarm.playAudio())
-   update()
+  update()
 }
 const update = () => {
   if (playState.value === '休息中') {
@@ -74,12 +98,24 @@ const update = () => {
   }
   start()
 }
+const distract = () => {
+  distracting.value = true
+  distractTimer = setInterval(() => {
+    distractTimeDisplay.value = timeFormated(++distractTime)
+  }, 1000)
+}
+
+const stopDistract = () => {
+  clearInterval(distractTimer)
+  distracting.value = false
+}
+
 const stop = () => {
   clearInterval(timer)
   going.value = false
 }
 const earlier = () => {
-  nowItem.totalTime = nowItem.id > 0 ? time - leftTime.value : rest - leftTime.value
+  nowItem.totalTime = nowItem.id > 0 ? time - leftTime - distractTime : rest - leftTime
   end(false)
 }
 
