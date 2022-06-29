fa4f1276199f7c50334b406d055122d555490097
applefi87 <werrtyyui@yahoo.com.tw>
2022-06-29 20:20:02 +0800

    已完成可過濾專案顯示(40)

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
 button(@click="clear") 清空
+| 專案:
+select(v-model="projectFilter")
+  option(:value="false") 全部
+  option(v-for="p in projectList" :value="p") {{ p }}
 table
   tr
     th 標題
@@ -7,23 +11,36 @@ table
     th 開始時間
     th 專心
     th 分心
-
-  tr(v-for="it in data.endLists")
+  tr(v-for="it in filtedList")
     td.title {{ it.title }}
     td.project {{ it.project }}
     td.startTime {{ it.startTime }}
     td.totalTime {{ it.totalTime }}
     td.distractTime {{ it.distractTime }}
-
-
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
 import { useDataStore } from '../stores/data.js'
 const data = useDataStore()
+// 清空紀錄
 const clear = () => {
-data.endLists=[]
+  data.endLists = []
 }
+
+// 建立專案清單供過過濾
+const projectList = computed(() => {
+  // 從完成清單取出有專案的專案名稱
+  const list = data.endLists.filter(it => it.project).map(it => it.project)
+  // 變成不重複名單
+  return [...new Set(list)]
+})
+const projectFilter = ref(false)
+
+// 過濾後顯示的專案內容
+const filtedList = computed(() => {
+  return projectFilter.value ? data.endLists.filter(it => it.project === projectFilter.value) : data.endLists
+})
 
 </script>
 
@@ -39,5 +56,14 @@ td,th
 .title
   width: 40%
 
+// 過濾相關
+  // 專案過濾用
+select
+  border: 1px solid black
+  border-radius: 3px
+  margin-left: 10px
+  padding: 0 5px
+  font-size: 20px
+  appearance: 
 </style>
  
